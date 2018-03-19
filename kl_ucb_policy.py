2e2c0a4978dac07351dd2fea2887e45c1e7f5113
Zakarya Ali <zakarya.ali@gmail.com>
2018-03-19 20:53:39 +0100

    refactoring kl_ucb_policy

--- kl_ucb_policy.py
+++ kl_ucb_policy.py
@@ -1,14 +1,13 @@
 import numpy as np
-#import math
 
 class KLUCBPolicy :
-    def __init__(self, K, DELTA):
+    def __init__(self, K, delta):
         self.K = K
-        self.DELTA = DELTA
+        self.delta = delta
         self.EPS = 10**(-12)
         self.reset()
 
-    def kl(self, p, q): #calculate kl-divergence
+    def kl_distance(self, p, q): #calculate kl-divergence
         result =  p * np.log(p/q) + (1-p)*np.log((1-p)/(1-q))
         return result
 
@@ -20,33 +19,33 @@ class KLUCBPolicy :
         self.N = np.zeros(self.K)
         self.S = np.zeros(self.K)
 
-    def getKLUCBUpper(self, k, n):
+    def get_klucb_upper(self, k, n):
         logndn = np.log(n)/self.N[k]
         arg1=self.S[k]/self.N[k]
         #print("arg1 =", arg1)
-        p = max(arg1, self.DELTA)
+        p = max(arg1, self.delta)
         if(p>=1):
             return 1
 
         converged = False
-        q = p + self.DELTA
+        q = p + self.delta
 
         for t in range(20):
-            f  = logndn - self.kl(p, q)
+            f  = logndn - self.kl_distance(p, q)
             df = - self.dkl(p, q)
 
             if(f*f < self.EPS):
                 converged = True
                 break
 
-        q = min(1 - self.DELTA , max(q - f / df, p + self.DELTA))
+        q = min(1 - self.delta , max(q - f / df, p + self.delta))
 
         if(not converged):
-            print("KL-UCB algorithm: Newton iteration in did not converge! p=" + str(p) + " logndn=" + str(logndn))
+            print("KL-UCB algorithm: Newton iteration did not converge!", "p=", p, "logndn=", logndn)
 
         return q
 
-    def selectNextArm(self):
+    def select_next_arm(self):
         n = np.sum(self.N)
         indices = np.zeros(self.K)
         for k in range(self.K):
@@ -54,11 +53,11 @@ class KLUCBPolicy :
                 return k
 
             #KL-UCB index
-            indices[k] = self.getKLUCBUpper(k, n)
+            indices[k] = self.get_klucb_upper(k, n)
 
-        targetArm = np.argmax(indices)
-        return targetArm
+        target_arm = np.argmax(indices)
+        return target_arm
 
-    def updateState(self, k, r):
+    def update_state(self, k, r):
         self.N[k] += 1
         self.S[k] += r
