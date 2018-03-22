d91c82eac0951eae8bb6d13dacfdb4b58b427955
thlevy <thom.levy@gmail.com>
2018-03-22 14:43:30 +0100

    Ajout scenario 2

--- KLUCB-V3.py
+++ KLUCB-V3.py
@@ -5,72 +5,53 @@ Created on Tue Feb  6 17:34:25 2018
 @author: Thomas
 """
 
-# KL UCB project
+# Algo UCB pour scenario 2
 
 #import pandas as pd
 import numpy as np
 import math
 import matplotlib.pyplot as plt
 
+#Params du scenario 2
+p = np.array([0.05, 0.02, 0.01, 0.05, 0.02, 0.01, 0.05, 0.02, 0.01, 0.1]) #Param des Bernoulli
+K= p.shape[0] #Nb de bras
+p_star = np.max(p)
+Delta= ( np.ones(K)*p_star ) - p
 
-#UCB avec 4 bras
-K=4
+T= 50000 #Nb max d'iterations
 
-p = np.array([0.05, 0.1, 0.05, 0.05]) #Param des Bernoulli
-delta=0.1
-Delta = np.array([0.05, 0, 0.05, 0.05]) #Delta avec moy optimale
 
-T= 5000 #Nb max d'iterations
-
+#UCB params initialization
 alpha=3
+N3 = np.zeros(K) #Nb de fois ou le bras K a été tiré
 
-N = np.array([0, 0, 0, 0]) #Nb de fois ou le bras K a été tiré
+Reward_UCB3 = np.zeros((K,T),dtype=np.int) #Recompense avec chaque bras
+Action_UCB3 = np.zeros((K,T),dtype=np.int) #Bras choisi a chaque étape
 
-Reward = np.zeros((K,T+1),dtype=np.int) #Recompense avec chaque bras
+UCB3 = np.zeros(K) #Gain UCB pour chaque bras
 
-Action = np.zeros((K,T+1),dtype=np.int) #Bras choisi a chaque étape
+#On commence en explorant les K bras au moins une fois
+for t in range(K):
+    N3[t]=1
+    Reward_UCB3[t,t]= np.random.binomial(1, p[t])
+    Action_UCB3[t,t]=1
 
-UCB= np.array([10., 10., 10., 10.]) #UCB pour chaque bras
-
-#On commence en explorant les 2 bras au moins une fois
-#A t=1
-
-#N[0]=1
-#Reward[0,1]= np.random.binomial(1, p[0])
-#Action[0,1]=1
-#
-##A t=2
-#N[1]=1
-#Reward[1,2]= np.random.binomial(1, p[1])
-#Action[1,2]=1
-
-for t in range(1,T+1):
+for t in range(K,T):
     #Added slight optimization (sum only up to t)
     for i in range(K):
-        if (N[i]>0):
-            UCB[i] = sum(Reward[i,:t])/N[i]+np.sqrt( (alpha*math.log(t))/(2*N[i]) )
+        UCB3[i] = np.sum(Reward_UCB3[i,:t])/N3[i]+np.sqrt( (alpha*math.log(t))/(2*N3[i]) )
             
-    #print("t & UCB :",t,UCB)
-    select = np.argmax(UCB)
+    select = np.argmax(UCB3)
     #print("t & select",t, select)
-    N[select]=N[select]+1
-    Action[select,t]=1
-    Reward[select,t]= np.random.binomial(1,p[select])
+    N3[select]=N3[select]+1
+    Action_UCB3[select,t]=1
+    Reward_UCB3[select,t]= np.random.binomial(1,p[select])
 
-# Now using argmax instead    
-#    if UCB[0]>= UCB[1]:
-#        N[0]=N[0]+1
-#        Reward[0,t]= np.random.binomial(1,p[0])
-#    else:
-#        N[1]=N[1]+1
-#        Reward[1,t]= np.random.binomial(1,p[1])        
+#print("t & Reward :",t,sum(Reward[0,:]),sum(Reward[1,:]),sum(Reward[0,:])+sum(Reward[1,:]) )
+cumReward=np.cumsum(Reward_UCB3,axis=1) #Reward accumulé pour chaque bras en fonction du temps
+totalReward=np.sum(cumReward,axis=0) #Reward sur tout les bras accumulé en fonction du temps
 
-print("t & Reward :",t,sum(Reward[0,:]),sum(Reward[1,:]),sum(Reward[0,:])+sum(Reward[1,:]) )
-
-
-cumReward=np.cumsum(Reward,axis=1) #Reward accumulé en fonction du temps
-
-totalReward=np.sum(cumReward,axis=0)
+print("t & Reward (total) :",t, totalReward[t-1] )
    
 fig=plt.figure(figsize=(12,8))
 ##fig=plt.figure()  
@@ -79,24 +60,22 @@ for i in range (K):
 ##ax1.plot(c_error,marker='.',linestyle='-',label='Online avec gradient à pas constant')
     ax1.plot(cumReward[i,:],linestyle='-',label='Gain avec bras '+str(i))
 #ax1.plot(totalReward[1,:],linestyle='-',label='Gain avec bras 2')
-ax1.plot(totalReward[:],linestyle='-',label='Gain total UCB')
+ax1.plot(totalReward[:],linestyle='-',label='Gain total')
 ax1.legend(loc='best')
 plt.show()
 
-totalAction=np.cumsum(Action,axis=1)
-#print("t, Nb de tirages sous-optimal, Pseudo-Regret :",t,totalAction[0,t],totalAction[0,t]*delta)
-print("t, Pseudo-Regret :",t,np.dot(Delta,totalAction[:,t]))
+totalAction=np.cumsum(Action_UCB3,axis=1) #Nb d'actions accumulé sur chaque bras
+cumRegret=np.dot(Delta,totalAction[:,:]) #Regret accumulé
 
-cumRegret=np.dot(Delta,totalAction[:,:])
+print("t & Pseudo-Regret :",t,cumRegret[T-1])
 
 fig=plt.figure(figsize=(12,8))
 ##fig=plt.figure()  
 ax1 = fig.add_subplot(1,1,1)
-##ax1.plot(c_error,marker='.',linestyle='-',label='Online avec gradient à pas constant')
-ax1.plot(cumRegret[:],linestyle='-',label='Pseudo-Regret avec UCB')
-#ax1.plot(totalReward[1,:],linestyle='-',label='Gain avec bras 2')
-#ax1.plot(totalReward[0,:]+totalReward[1,:],linestyle='-',label='Gain total UCB')
+ax1.plot(cumRegret[:],linestyle='-',label='Pseudo-Regret avec UCB avec alpha=3')
 ax1.legend(loc='best')
+ax1.grid()
+#ax1.xscale('log')
 plt.show()
 
 
